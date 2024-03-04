8d8f24a9731082667ae66e6a11b0a25e6246d684
dafnesvc <43295158+dafnesvc@users.noreply.github.com>
2024-03-03 21:35:21 -0600

    Update 001_Variables.py

--- 001_Variables.py
+++ 001_Variables.py
@@ -1,4 +1,4 @@
-num1=12
-num2=11
-sum=num1+num2
-print(sum)
+num1=12 #Se le asigna el valor 12 a la variable num1
+num2=11 #Se le asigna el valor 11 a la variable num2
+sum=num1+num2 #La variable sum guardara la suma de las variables num1 y num2
+print(sum)#print imprimirá el valor de sum 
