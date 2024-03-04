39f3f0f0560e236cfe47129032dabc10f9ac2c58
dafnesvc <43295158+dafnesvc@users.noreply.github.com>
2024-03-03 22:59:16 -0600

    Update 006_Operaciones_aritméticas.py

--- 006_Operaciones_aritméticas.py
+++ 006_Operaciones_aritméticas.py
@@ -1,24 +1,25 @@
+#21310176 Dafne Sarahi Villanueva Ceja.
 #Almacena en una variable una suma que de como resultado el número 87. Es obligatorio incluir en dicha suma los números 20 y 23
-suma= 20+23+44
-print(suma)
-
+suma= 20+23+44 #La variable suma almacena datos tipo int donde se suman con el operador + , en comparación con datos tipo string el op. + concatena
+  print(suma) #imprime el resultado de la variable "suma" (la suma de los datos tipo int:"87") 
+  
 #Almacena en una variable una resta que de como resultado el número negativo -87. 
 #Es obligatorio incluir en dicha resta los números positivos 20 y 23
-resta=20-23-44-40
-print(resta)
+resta=20-23-44-40 #La variable "resta" guarda datos tipo int donde con el operador "-" resta los datos.
+  print(resta) #imprime el resultado de la variable "resta" 
 
 #Almacena en una variable una multiplicación que de como resultado el número 870. Es obligatorio incluir}
 #en dicha multiplicación los números 20 y 23.
-mult=(10*87)
-print(int(mult))
+mult=(10*87) #la variable "mult" almacena datos tipo int donde se multiplican con el operador "*"
+  print(int(mult)) #print imprime el resultado entero de la variable "mult"
 
 #Almacena en una variable una división que de como resultado el número 10 (no hace falta que sea 10 exacto, puede tener decimales).
 #Es obligatorio incluir en dicha división los números 5000 y 230.
-div=5000/230/2.17391
-print(int(div))
+div=5000/230/2.17391 #la variable "div" almacena datos tipo int donde se dividen con el operador "/"
+print(int(div)) #print imprime el resultado entero de la variable "div" 
 
 #A la siguiente operación añade los símbolos aritméticos básicos (+, -, *, /). Debes utilizar los cuatro sin repertirlos. 
 #Uno para cada espacio: operacion = 10 ____ 5 ____ 15 ____ 17
-op=10/5+15-17
-print(op)
+op=10/5+15-17 #en la variable op se guardan datos tipo int con operacion
+print(op) #imprime el resultado de la operación de la variable "op"
 
