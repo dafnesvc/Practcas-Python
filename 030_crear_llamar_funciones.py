b3b7c4b9cf134bf24be39499a26ac33916c6debe
Dafne Villanueva <cejasarahi123@gmail.com>
2024-03-03 20:10:59 -0600

    Crear y llamar funciones

---
+++ 030_crear_llamar_funciones.py
@@ -0,0 +1,15 @@
+#https://www.programacionfacil.org/cursos/python_basico/capitulo_34_funciones_python.html
+
+'''
+Crea una función que realice una suma. Para ello, tendrás que añadir dos argumentos(numero1 y numero2). 
+En su interior, especificarás un print() que muestre el resultado de la suma. Deberás hacer tres llamadas que como resultado de la suma den los valores 30, 50 y 57000.
+Los números que introduzcas en la llamada pueden ser los que quieras siempre que coincidan los resultados en el print().
+
+'''
+
+def suma(num1, num2):
+	print(num1 + num2)
+
+suma(10, 20)
+suma(20, 30)
+suma(50000, 7000)
