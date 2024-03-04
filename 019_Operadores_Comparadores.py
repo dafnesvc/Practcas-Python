4c672181b0928e10701d67ff373187de99282561
Dafne Villanueva <cejasarahi123@gmail.com>
2024-03-03 19:50:17 -0600

    op. Comparador !=,<=,>=

---
+++ 019_Operadores_Comparadores.py
@@ -0,0 +1,24 @@
+#Cambia el operador para que la condición sea True.
+
+num1 = 15
+num2 = 20
+
+if num1 != num2:
+	print('Se ejecuta el if.')
+	
+#Cambia el operador para que la condición sea True.
+
+num1 = 1450
+num2 = 60
+
+if num1 > num2:
+	print('Se ejecuta el if.')
+
+#Haz que el siguiente condicional se convierta en False sin cambiar el operador.
+
+num1 = 60
+num2 = 60
+
+if num1 != num2:
+	print('Se ejecuta el if.')
+	
