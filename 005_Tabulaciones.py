41767224ff2e0aa2121195c1e5e4f45732f8fca0
dafnesvc <43295158+dafnesvc@users.noreply.github.com>
2024-03-03 22:39:57 -0600

    Update 005_Tabulaciones.py

--- 005_Tabulaciones.py
+++ 005_Tabulaciones.py
@@ -1,5 +1,6 @@
+#21310176 Dafne Sarahi Villanueva Ceja.
 #Escribe el contenido (strings) de todos estos print() en uno solo con un string. Debes utilizar los saltos de línea para que 
 #quede cada lenguaje de programación escrito en una línea. Al final, añade dos lenguajes más. Además, quiero que añadas un guion
 #al principio de cada fila y un punto al final. 
-lenguajes="-Python.\n-JavaScript.\n-Java.\n-PHP.\n-TypeScript.\n-C#.\n-C++."
-print(lenguajes)
+lenguajes="-Python.\n-JavaScript.\n-Java.\n-PHP.\n-TypeScript.\n-C#.\n-C++." #la variable lenguajes guarda datos tipo string "" con salto de linea "\n"
+print(lenguajes)#imprime los datos de la variable lenguajes con tabulaciones
