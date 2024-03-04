9f1105a3eda526cf712066d4387948e2667262b4
Dafne Villanueva <cejasarahi123@gmail.com>
2024-03-03 20:05:13 -0600

    bucle for con range

---
+++ 026_bucle_for_con_range.py
@@ -0,0 +1,5 @@
+#https://www.programacionfacil.org/cursos/python_basico/capitulo_30_bucle_for_python_2.html
+
+#Crea un bucle for con un range() que vaya desde el valor 7 hasta el valor 700 en saltos de 100. Basta con que imprimas el valor de cada iteración.
+for x in range(7, 700, 100):
+	print(x)
