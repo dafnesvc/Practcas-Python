66157f6f0d2ba7ac408c2d7d4d438f71a3ad993b
dafnesvc <43295158+dafnesvc@users.noreply.github.com>
2024-03-03 19:29:39 -0600

    operaciones +,-,*,/

---
+++ 006_Operaciones_aritméticas.py
@@ -0,0 +1,24 @@
+#Almacena en una variable una suma que de como resultado el número 87. Es obligatorio incluir en dicha suma los números 20 y 23
+suma= 20+23+44
+print(suma)
+
+#Almacena en una variable una resta que de como resultado el número negativo -87. 
+#Es obligatorio incluir en dicha resta los números positivos 20 y 23
+resta=20-23-44-40
+print(resta)
+
+#Almacena en una variable una multiplicación que de como resultado el número 870. Es obligatorio incluir}
+#en dicha multiplicación los números 20 y 23.
+mult=(10*87)
+print(int(mult))
+
+#Almacena en una variable una división que de como resultado el número 10 (no hace falta que sea 10 exacto, puede tener decimales).
+#Es obligatorio incluir en dicha división los números 5000 y 230.
+div=5000/230/2.17391
+print(int(div))
+
+#A la siguiente operación añade los símbolos aritméticos básicos (+, -, *, /). Debes utilizar los cuatro sin repertirlos. 
+#Uno para cada espacio: operacion = 10 ____ 5 ____ 15 ____ 17
+op=10/5+15-17
+print(op)
+
