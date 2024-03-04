a9b9fba399d4495bf9733c02426ff5089dcb6509
dafnesvc <43295158+dafnesvc@users.noreply.github.com>
2024-03-03 19:26:39 -0600

    Concatenación

---
+++ 003_Concatenación.py
@@ -0,0 +1,26 @@
+#Concatena dos strings en una variable. Puedes comprobar el resultado con un print().#
+string="Hello"+"Bye"
+print(string) 
+
+#Concatena dos variables con strings en una tercera variable.
+var1="Hola"
+var2=" Adiós"
+var3=var1+var2
+print(var3)
+
+#Concatena una variable con string y un string directamente en un print().
+v4="Bonjour"
+print(v4+' '+"hi")
+
+#Escribe tu nombre y apellidos separados en variables. Después concatena estas variables dentro de otra.
+#No te olvides de concatenar los espacios entre el nombre y cada apellido. Finalmente, imprime el resultado.
+nom="Dafne"
+apellido_p="Villanueva"
+apellido_m="Ceja"
+nom_compl=nom+' '+apellido_p+ ' '+apellido_m
+print(nom_compl)
+
+#Concatena dos números, no los sumes.
+num1="2"
+num2="3"
+print(num1+num2)
