768568710b4e50350d11269ba732536ed0595f69
Dafne Villanueva <cejasarahi123@gmail.com>
2024-03-03 20:07:24 -0600

    Diccionario

---
+++ 027_diccionarios.py
@@ -0,0 +1,18 @@
+#https://www.programacionfacil.org/cursos/python_basico/capitulo_31_diccionarios_python_1.html
+
+#Del diccionario teclado2 del capítulo, muestra los elementos Modelo y Precio con presentación en un print(). El resultado será esto en la consola:
+
+
+teclado1 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'HyperX Alloy FPS Pro',
+	'Precio': '89,99'
+}
+
+teclado2 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'Corsair K55 RGB',
+	'Precio': '59,99'
+}
+
+print('El modelo', teclado2['Modelo'], 'cuesta', teclado2['Precio'], '$.')
