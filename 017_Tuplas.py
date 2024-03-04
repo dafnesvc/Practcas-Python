10fb81a7fcd3f334cc788fa43291f6b08623093a
Dafne Villanueva <cejasarahi123@gmail.com>
2024-03-03 19:47:26 -0600

    creacion de tuplas

---
+++ 017_Tuplas.py
@@ -0,0 +1,10 @@
+#Imprime la segunda posición de esta tupla.
+colores = ('rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja')
+print(colores[1])
+
+#Utiliza los símbolos de suma y resta para obtener el resultado 25 a partir de los elementos de la siguiente 
+#tupla en una variable llamada operacion.
+numeros = (10, 1, 5, 11)
+print(numeros[0] + numeros[3] + numeros[2] - numeros[1])
+
+ 
