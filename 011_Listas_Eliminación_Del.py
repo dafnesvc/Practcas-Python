542325e52cc9688cd55c27ed17dd2b624652c1d0
dafnesvc <43295158+dafnesvc@users.noreply.github.com>
2024-03-03 23:23:21 -0600

    Update 011_Listas_Eliminación_Del.py

--- 011_Listas_Eliminación_Del.py
+++ 011_Listas_Eliminación_Del.py
@@ -1,10 +1,11 @@
+#21310176 Dafne Sarahi Villanueva Ceja.
 #De esta lista, elimina los colores 'azul', 'marrón', 'negro' y 'rosa'. Debes utilizar al menos una vez las posiciones 
 #negativas para eliminar un color. Después, imprime la lista para ver los colores que se han eliminado.
 
-colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
-del colores[1]#azul
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']# A la variable colores se le asigna una lista de datos tipo string
+del colores[1]#Para eliminar de esta lista un elemento lo puedes hacer con del y el número de posición en la lista, en este caso se elimina el "azul"
 del colores[3]#marron
 del colores[4]#negro
 del colores[-3]#rosa
-print(colores)
+print(colores)#imprime la lista con los nuevos valores 
 
