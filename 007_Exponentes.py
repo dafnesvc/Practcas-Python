b9683f2d06b98a0a8b5612abe171a1edc0ff45b8
dafnesvc <43295158+dafnesvc@users.noreply.github.com>
2024-03-03 23:01:35 -0600

    Update 007_Exponentes.py

--- 007_Exponentes.py
+++ 007_Exponentes.py
@@ -1,4 +1,5 @@
+#21310176 Dafne Sarahi Villanueva Ceja.
 #Utilizando solo el número 2 (cuantas veces necesites) y sin utilizar exponentes, debes obtener el resultado 1024.
 #Pista: empieza por esto y añade todos los números 2 que falten: mil_veinticuatro = 2 * 2 ....
-resultado=2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2
-print(resultado)
+resultado=2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 #Se multiplica el 2 tantas veces hasta llegar 1024
+print(resultado) #imprime el dato de la variable "resultado"
