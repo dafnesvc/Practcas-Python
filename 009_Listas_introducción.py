58e90df2e5a5dc7f7084089437413172e77c2581
dafnesvc <43295158+dafnesvc@users.noreply.github.com>
2024-03-03 19:31:52 -0600

    Listas introducción

---
+++ 009_Listas_introducción.py
@@ -0,0 +1,12 @@
+#De la siguiente lista, ¿qué color está en la posición 3?
+smartphones = ["rojo", "azul", "verde", "amarillo", "marrón", "lila", "negro", "rosa"]
+print(smartphones[3])
+
+#Crea una lista que contenga los siguientes valores en las posiciones indicadas.
+#"uno" en la posición 4.
+#"dos" en la posición 1.
+#"tres" en la posición 0.
+#"cuatro" en la posición 3.
+#"cinco" en la posición 2.
+list=["tres","dos","cinco","cuatro","uno"]
+print(list)
