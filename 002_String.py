79d29e6af4c21f1d725a18c8a5b7f08be4b710a3
dafnesvc <43295158+dafnesvc@users.noreply.github.com>
2024-03-03 22:06:22 -0600

    Update 002_String.py

--- 002_String.py
+++ 002_String.py
@@ -1,4 +1,6 @@
-var1="'print()' se utiliza para imprimir valores en la consola."
-var2='"print()" se utiliza para imprimir valores en la consola.'
-print(var1)
-print(var2)
+#21310176 Dafne Sarahi Villanueva Ceja.
+#Python nos deja escribir strings entre "" y ''
+var1="'print()' se utiliza para imprimir valores en la consola."#no se puede utilizar doble comillas dentro de "" por lo que se le asigna '' y se imprimiran 
+var2='"print()" se utiliza para imprimir valores en la consola.'# si quieres imprimir literalmente "" utilizamos para el string las ''
+print(var1)#imprime la variable1 con dato tipo string
+print(var2)#imprime la var2 con dato tipo string
