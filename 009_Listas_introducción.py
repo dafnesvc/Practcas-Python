ead28fb9be72d3d2a777eefdcee286fd17158a3b
dafnesvc <43295158+dafnesvc@users.noreply.github.com>
2024-03-03 23:16:09 -0600

    Update 009_Listas_introducción.py

--- 009_Listas_introducción.py
+++ 009_Listas_introducción.py
@@ -1,6 +1,7 @@
+# 21310176 Dafne Sarahi Villanueva Ceja
 #De la siguiente lista, ¿qué color está en la posición 3?
-smartphones = ["rojo", "azul", "verde", "amarillo", "marrón", "lila", "negro", "rosa"]
-print(smartphones[3])
+smartphones = ["rojo", "azul", "verde", "amarillo", "marrón", "lila", "negro", "rosa"]#se guarda en la variable smartphone una lista con datos tipo string
+print(smartphones[3]) #imprime el dato en la posición 3 de la variable smartphones "amarillo"
 
 #Crea una lista que contenga los siguientes valores en las posiciones indicadas.
 #"uno" en la posición 4.
@@ -8,5 +9,5 @@ print(smartphones[3])
 #"tres" en la posición 0.
 #"cuatro" en la posición 3.
 #"cinco" en la posición 2.
-list=["tres","dos","cinco","cuatro","uno"]
-print(list)
+list=["tres","dos","cinco","cuatro","uno"] #la lista comienza desde el valor 0,1,2,3,4
+print(list) #imprime la lista de la variable "list"
