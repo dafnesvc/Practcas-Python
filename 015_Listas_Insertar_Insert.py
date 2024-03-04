d977e9d8b789dd4b46a289786f6a2fd496a36aad
dafnesvc <43295158+dafnesvc@users.noreply.github.com>
2024-03-03 21:56:22 -0600

    Update 015_Listas_Insertar_Insert.py

--- 015_Listas_Insertar_Insert.py
+++ 015_Listas_Insertar_Insert.py
@@ -1,7 +1,7 @@
 #Añade a la siguiente lista los colores 'magenta' y 'turquesa' utilizando insert(). Tendrás que posicionar 'magenta' en la posición 
 #siguiente a la de 'lila' y 'turquesa' en la penúltima posición. Deberás hacer esto utilizando posiciones de lista negativas.
 
-colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
-colores.insert(-4,'magenta')
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja'] # A la variable "colores" se le asigna una lista utilizando la "," con datos tipo string
+colores.insert(-4,'magenta')#El método insert() funciona con dos parámetros, primero el número de posición donde quieres posicionar el elemento y después, el elemento a añadir
 colores.insert(-1,'turquesa')
-print(colores)
+  print(colores)#El print imprimirá una nueva lista con los nuevos valores agregados "magenta y turquesa"
