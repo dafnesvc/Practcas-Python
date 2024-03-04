691999f188c15708980e7a3d9f01e2fd5ef75810
dafnesvc <43295158+dafnesvc@users.noreply.github.com>
2024-03-03 22:34:29 -0600

    Update 004_Métodos_UpperLowerTitle.py

--- 004_Métodos_UpperLowerTitle.py
+++ 004_Métodos_UpperLowerTitle.py
@@ -1,14 +1,15 @@
+#21310176 Dafne Sarahi Villanueva Ceja.
 #Pon en mayúsculas la primera letra de cada palabra de este nombre utilizando uno de los métodos del capítulo:
 #"enrique barros fernández"
-nom="enrique barros fernández".title()
-print(nom)
+nom="enrique barros fernández".title()# el método Title sirve para transformar la primera letra de cada palabra en mayúscula.
+  print(nom)#imprime la variable nom
 
 #Deja esta frase totalmente en letras minúsculas utilizando uno de los métodos del capítulo:
 #"Esta Es Una Frase Para Ser Formateada.
-frase="Esta Es Una Frase Para Ser Formateada".lower()
-print(frase)
+frase="Esta Es Una Frase Para Ser Formateada".lower()#El método lower hace que todas las letras se conviertan en minúsculas 
+  print(frase)#imprime el valor de la variable frase 
 
 #Deja esta frase totalmente en letras mayúsculas:
 #"Esta Es Una Frase Para Ser Formateada.
-frase2="Esta Es Una Frase Para Ser Formateada.".upper()
-print(frase2)
+frase2="Esta Es Una Frase Para Ser Formateada.".upper() #Con este método conseguimos convertir todos y absolutamente todos los caracteres de un string en mayúsculas
+  print(frase2) imprime el valo de la variable frase2
