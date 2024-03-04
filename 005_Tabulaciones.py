57486211d3574585bd36f2f0029636348a76ef15
dafnesvc <43295158+dafnesvc@users.noreply.github.com>
2024-03-03 19:28:46 -0600

    Tab. \n (lista)

---
+++ 005_Tabulaciones.py
@@ -0,0 +1,5 @@
+#Escribe el contenido (strings) de todos estos print() en uno solo con un string. Debes utilizar los saltos de línea para que 
+#quede cada lenguaje de programación escrito en una línea. Al final, añade dos lenguajes más. Además, quiero que añadas un guion
+#al principio de cada fila y un punto al final. 
+lenguajes="-Python.\n-JavaScript.\n-Java.\n-PHP.\n-TypeScript.\n-C#.\n-C++."
+print(lenguajes)
