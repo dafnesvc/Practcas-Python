83c85dd0bd5303d1f7f87c3ea4fb29dbb325ba83
dafnesvc <43295158+dafnesvc@users.noreply.github.com>
2024-03-03 23:07:52 -0600

    Update 008_Float.py

--- 008_Float.py
+++ 008_Float.py
@@ -1,3 +1,5 @@
+#21310176 Dafne Sarahi Villanueva Ceja.
 #Redondea con cinco decimales el número 17.567383292929200234.
-num=17.567383292929200234
-print(round(num,5))
+num=17.567383292929200234 #los datos con números decimales son datos tipo float 
+#el método "round()" redondea los números con decimales.
+  print(round(num,5)) #se escribe el metodo round(nombre de la variable, número de decimales deseados)
