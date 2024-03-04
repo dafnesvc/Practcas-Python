94710b2404b011da447f937ed32753a4335850ad
Dafne Villanueva <cejasarahi123@gmail.com>
2024-03-03 19:46:43 -0600

    Metodo Sort

---
+++ 016_Metodo_Sort.py
@@ -0,0 +1,10 @@
+#Ordena la siguiente lista en orden alfabético descendente (de la letra 'z' a la 'a').
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón','lila', 'negro', 'rosa', 'blanco', 'naranja']
+colores.sort(reverse=True) #el True solo funciona si lo escribes con la letra T mayúscula.
+print(colores)
+
+#Ascendente (a-z)
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón','lila', 'negro', 'rosa', 'blanco', 'naranja']
+colores.sort() 
+print(colores)
