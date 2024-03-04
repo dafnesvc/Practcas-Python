9f94fdfa2c1cbd4b6f3a317029aeb7a16c633998
Dafne Villanueva <cejasarahi123@gmail.com>
2024-03-03 19:43:12 -0600

    Eliminacion POP

---
+++ 013_Listas_Eliminacion_Pop.py
@@ -0,0 +1,5 @@
+#Elimina de la siguiente lista los elementos 'azul' y 'blanco'. Solo puedes eliminarlos utilizando el método pop(). Además,
+#tendrás que guardar esos dos colores en una nueva lista.
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+almacena_valor = [colores.pop(1),colores.pop(-2)]
+print('El color eliminado y almacenado es:',almacena_valor)
