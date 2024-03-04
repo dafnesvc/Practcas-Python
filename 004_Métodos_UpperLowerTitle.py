20859ee0a353889741a6f50192013434347b195e
dafnesvc <43295158+dafnesvc@users.noreply.github.com>
2024-03-03 19:27:18 -0600

    Upper Title Lower

---
+++ 004_Métodos_UpperLowerTitle.py
@@ -0,0 +1,14 @@
+#Pon en mayúsculas la primera letra de cada palabra de este nombre utilizando uno de los métodos del capítulo:
+#"enrique barros fernández"
+nom="enrique barros fernández".title()
+print(nom)
+
+#Deja esta frase totalmente en letras minúsculas utilizando uno de los métodos del capítulo:
+#"Esta Es Una Frase Para Ser Formateada.
+frase="Esta Es Una Frase Para Ser Formateada".lower()
+print(frase)
+
+#Deja esta frase totalmente en letras mayúsculas:
+#"Esta Es Una Frase Para Ser Formateada.
+frase2="Esta Es Una Frase Para Ser Formateada.".upper()
+print(frase2)
