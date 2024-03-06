1209df82e855b1f9f377da0731f630f3902b93b2
dafnesvc <43295158+dafnesvc@users.noreply.github.com>
2024-03-06 17:49:28 -0600

    Insertar en listas por metodo append

--- 014_Listas_Insertar_Append.py
+++ 014_Listas_Insertar_Append.py
@@ -1,5 +1,6 @@
+#Dafne Villanueva 21310176.
 #Añade los colores 'fuxia' y 'celeste' a la lista.
-colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
-colores.append('fuxia')
-colores.append('celeste')
-print(colores)
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja'] #la variable colores gurada lista 
+colores.append('fuxia')#con el metodo append se agregan nuevos valores a la variable colores 
+colores.append('celeste') # a la variable colores se le agrega el nuevo valor "celeste"
+print(colores)#imprime la nueva lista con los valores agregados "fuxia y celeste"
