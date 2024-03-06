36677e073e9d193b0af591ba3fc05e6bdf529b09
dafnesvc <43295158+dafnesvc@users.noreply.github.com>
2024-03-06 17:23:38 -0600

    Update and rename 012_Listas_Eliminación_Remove.py to 013_Listas_Eliminación_Remove.py

---
+++ 013_Listas_Eliminación_Remove.py
@@ -0,0 +1,7 @@
+#Dafne Villanueva 21319176
+#Elimina de la siguiente lista los elementos 'amarillo' y 'rojo'. Solo puedes eliminarlos utilizando el método remove().
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja'] #se crea lista en la varible colores
+colores.remove('amarillo')# de la variable "colores" se elimina de la lista "amarillo" 
+colores.remove('rojo') # de la variable "colores" se elimina de la lista "rojo" 
+print(colores)#imprime la lista de colores sin los ya eliminados "amarillo y rojo"
