fc163e1d4be45f85c899144f402288940f54c714
dafnesvc <43295158+dafnesvc@users.noreply.github.com>
2024-03-07 18:41:16 -0600

    Fechas con datetime

---
+++ 041_fechas_con_datetime.py
@@ -0,0 +1,10 @@
+import datetime
+
+print("\nFecha actual")
+fecha = datetime.datetime.now()
+print(fecha)
+
+#Fecha personalizada
+print("\nFecha personalizada")
+fecha = datetime.datetime(2020, 9, 29, 19, 45)
+print(fecha, '\n')
