c71471fc873fe04bf862db65750a1fe4b6cb8886
dafnesvc <43295158+dafnesvc@users.noreply.github.com>
2024-03-07 18:42:45 -0600

    Manejo de excepciones

---
+++ 045_manejo_excepciones.py
@@ -0,0 +1,7 @@
+#https://www.programacionfacil.org/cursos/python_basico/capitulo_51_manejo_excepciones_python.html
+variable = "Correcto."
+
+try:
+	print(variable)
+except:
+	print("La variable no está declarada.")
